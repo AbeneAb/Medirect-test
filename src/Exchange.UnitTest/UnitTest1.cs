fd486a2e184bc88b4f585ef5ca5111b61c651fe8
Abenezer B <abayu@redzone.com>
2022-05-22 14:33:21 +0300

    Add project files.

---
+++ src/Exchange.UnitTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Exchange.UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ src/services/Exchange/Exchange.FunctionalTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Exchange.FunctionalTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
